13529db829e4982c1a7369b6f40a266dbb4c7e05
João Gabriel Alves <joao.alves.1129911@sga.pucminas.br>
2023-03-08 13:35:19 -0300

    atv pessoa & conta

---
+++ atividades/conta.cs
@@ -0,0 +1,43 @@
+/*
+Crie uma classe Conta, que representa uma conta bancária com um atributo saldo do tipo float e um atributo criacao do tipo DateTime. 
+
+Em seguida, implemente um atributo chamado numero, que representa o número da conta bancária, e deve ser um número sequencial, gerado automaticamente, a cada vez que uma instância de Conta é criada.
+
+Finalmente, implemente uma classe MainClass para mostrar o funcionamento da sua classe Conta.
+*/
+
+using System;
+
+class Conta
+{
+	public int numero;
+	public float saldo;
+	public DateTime criacao;
+
+	public static int contador;
+
+	public Conta(float saldo)
+	{
+		this.numero = ++Conta.contador;
+		if(saldo >= 0)
+			this.saldo = saldo;
+		this.criacao = DateTime.Now;
+	}
+}
+
+class MainClass
+{
+	public static void Main()
+	{
+		Conta[] listaContas = new Conta[5];
+		for (int i = 0; i < 5; i++)
+		{
+			Random rnd = new Random();
+			float num = rnd.Next();
+			listaContas[i] = new Conta((num));
+			Console.WriteLine($"Número da Conta: {listaContas[i].numero}");
+			Console.WriteLine($"Saldo da Conta: {listaContas[i].saldo}");
+			Console.WriteLine($"Criação da Conta: {listaContas[i].criacao}\n");
+		}
+	}
+}
---
+++ atividades/pessoa.cs
@@ -0,0 +1,75 @@
+/*
+Implemente uma classe Pessoa em C# que armazena os seguintes atributos.
+
+             Nome: do tipo texto e armazena o nome completo da pessoa,
+             Endereço: do tipo texto e armazena o endereço completo da pessoa,
+             Telefone: do tipo texto e armazena o DDD e o número do telefone da pessoa,
+
+E o seguinte método:
+          primeiroNome( ): retorna elemento do tipo texto
+
+Este método retorna o primeiro nome da pessoa. Por enquanto não se preocupe com a ocorrência de nomes compostos.
+
+Por enquanto, os atributos devem ser acessíveis diretamente, sem a necessidade de métodos de acesso.
+
+Implemente um construtor default e um construtor com parâmetros que inicialize os três atributos com valores fornecidos pelo usuário.
+
+Finalmente, implemente uma classe MainClass para mostrar o funcionamento da sua classe Pessoa.
+*/
+
+using System;
+
+class Pessoa
+{
+	public string nome;
+	public string endereco;
+	public string telefone;
+
+	public Pessoa(String nome, String endereco, String telefone)
+	{
+		if (nome.Length >= 6)
+		{
+			this.nome = nome;
+		} else {
+			this.nome = "Sem nome";
+		}
+		if (endereco.Length >= 6)
+		{
+			this.endereco = endereco;
+		} else {
+			this.endereco = "Sem endereço";
+		}
+		if (telefone.Length >= 6)
+		{
+			this.telefone = telefone;
+		} else {
+			this.telefone = "Sem telefone"
+		}
+	}
+
+	public string primeiroNome()
+	{
+		if (string.IsNullOrWhiteSpace(nome))
+		{
+			return "";
+		}
+
+		string[] nomeSeparado = nome.Split(" ");
+		return nomeSeparado[0];
+	}
+}
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+		Pessoa pessoa = new Pessoa();
+		pessoa.nome = "Hiro Protagonist";
+		pessoa.endereco = "Baker Street 221B - London (UK)";
+		pessoa.telefone = "555 567 5309";
+
+		Console.Write($"{pessoa.nome} mora em {pessoa.endereco} e seu # é {pessoa.telefone}.");
+
+		Console.Write($"\nSeu primeiro nome é: {(pessoa.primeiroNome())}");
+	}
+}
